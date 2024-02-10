233dc390a63a1af3d78b0fd23c4d91c39641722e
Martinchx <martincruz1426@gmail.com>
2024-02-10 17:10:44 -0300

    Unused code in class Server removed

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,44 +1,28 @@
 import express, { Router } from "express";
-import path from "path";
 
 interface Options {
   port: number;
   routes: Router;
-  public_path?: string;
 }
 
 export class Server {
   public readonly app = express();
   private serverListener?: any;
   private readonly port: number;
-  private readonly publicPath: string;
   private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, routes, public_path = "public" } = options;
+    const { port, routes } = options;
     this.port = port;
-    this.publicPath = public_path;
     this.routes = routes;
   }
 
   async start() {
-    //* Middlewares
-    this.app.use(express.json()); // raw
-    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+    /* Middlewares */
+    this.app.use(express.json());
 
-    //* Public Folder
-    this.app.use(express.static(this.publicPath));
-
-    //* Routes
+    /* Routes */
     this.app.use(this.routes);
-
-    //* SPA /^\/(?!api).*/  <== Only if it does not start with the word api
-    this.app.get("*", (req, res) => {
-      const indexPath = path.join(
-        __dirname + `../../../${this.publicPath}/index.html`
-      );
-      res.sendFile(indexPath);
-    });
 
     this.serverListener = this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`);
