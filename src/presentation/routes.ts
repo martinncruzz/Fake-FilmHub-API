8cc5a37f557010b4b10c87a1b55ab33ba4df7f04
Martinchx <martincruz1426@gmail.com>
2024-02-10 17:12:32 -0300

    Product routes added

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
+import { ProductRoutes } from "./routes/product.routes";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
-    //* Define routes
-    // router.use('/api/example', /* ExampleRoutes.routes */ );
+    router.use("/api/products", ProductRoutes.routes);
 
     return router;
   }
