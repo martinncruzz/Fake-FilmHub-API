bf417cf137f690873ff39985c723f6cc6a0b5e26
Martinchx <martincruz1426@gmail.com>
2024-02-12 03:21:46 -0300

    Migrate Routes for Genres, updating endpoints and route handling

---
+++ src/presentation/routes/genre.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { GenreService } from "../services";
+import { GenreController } from "../controllers/genre.controller";
+
+export class GenreRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const genreService = new GenreService();
+    const genreController = new GenreController(genreService);
+
+    router.get("/get-genres", genreController.getGenres);
+    router.get("/get-genre/:id", genreController.getGenreById);
+    router.post("/create-genre", genreController.createGenre);
+    router.put("/update-genre/:id", genreController.updateGenre);
+    router.delete("/delete-genre/:id", genreController.deleteGenre);
+
+    return router;
+  }
+}
