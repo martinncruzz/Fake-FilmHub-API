95d55f4cfe7d8fc76a930fefe1da460f7d3915c1
Martinchx <martincruz1426@gmail.com>
2024-02-16 17:15:11 -0300

    MovieGenre relation logic implemented in Genre Service and refactored code to reduce duplication and enhance reusability.

--- src/presentation/services/genre.service.ts
+++ src/presentation/services/genre.service.ts
@@ -10,23 +10,34 @@ export class GenreService {
   constructor() {}
 
   async getGenres() {
-    const genres = await prisma.genre.findMany();
-    return genres;
-  }
-
-  async getGenreById(genreIdDto: GenreIdDto) {
-    const genreFound = await prisma.genre.findFirst({
-      where: {
-        genre_id: genreIdDto.genre_id,
+    const genres = await prisma.genre.findMany({
+      include: {
+        movies: {
+          include: {
+            movie: true,
+          },
+        },
       },
     });
 
-    if (!genreFound) throw CustomError.notFound("Genre not found");
+    return genres.map((genre) => ({
+      ...genre,
+      movies: genre.movies.map((movieData) => movieData.movie),
+    }));
+  }
 
-    return genreFound;
+  async getGenreById(genreIdDto: GenreIdDto) {
+    const genreFound = await this.validateGenreExistence(genreIdDto.genre_id);
+
+    return {
+      ...genreFound,
+      movies: genreFound.movies.map((movieData) => movieData.movie),
+    };
   }
 
   async createGenre(createGenreDto: CreateGenreDto) {
+    await this.validateGenreName(createGenreDto.name);
+
     try {
       const genre = await prisma.genre.create({
         data: createGenreDto,
@@ -42,13 +53,9 @@ export class GenreService {
   async updateGenre(updateGenreDto: UpdateGenreDto) {
     const { genre_id, ...updatedGenreDtoData } = updateGenreDto;
 
-    const genreExists = await prisma.genre.findFirst({
-      where: {
-        genre_id: updateGenreDto.genre_id,
-      },
-    });
+    await this.validateGenreExistence(genre_id);
 
-    if (!genreExists) throw CustomError.notFound("Genre not found");
+    if (updateGenreDto.name) await this.validateGenreName(updateGenreDto.name);
 
     try {
       const updatedGenre = await prisma.genre.update({
@@ -66,23 +73,90 @@ export class GenreService {
   }
 
   async deleteGenre(genreIdDto: GenreIdDto) {
-    const genreExists = await prisma.genre.findFirst({
-      where: {
-        genre_id: genreIdDto.genre_id,
-      },
-    });
+    await this.validateGenreExistence(genreIdDto.genre_id);
 
-    if (!genreExists) throw CustomError.notFound("Genre not found");
+    await this.validateGenreRelations(genreIdDto.genre_id);
 
     try {
-      await prisma.genre.delete({
-        where: { genre_id: genreIdDto.genre_id },
-      });
+      await prisma.$transaction([
+        prisma.movieGenre.deleteMany({
+          where: {
+            genre_id: genreIdDto.genre_id,
+          },
+        }),
+        prisma.genre.delete({
+          where: { genre_id: genreIdDto.genre_id },
+        }),
+      ]);
 
       return true;
     } catch (error) {
       console.log(error);
       throw CustomError.internalServer(`${error}`);
     }
+  }
+
+  private async validateGenreExistence(genre_id: number) {
+    const genreExists = await prisma.genre.findFirst({
+      where: {
+        genre_id: genre_id,
+      },
+      include: {
+        movies: {
+          include: {
+            movie: true,
+          },
+        },
+      },
+    });
+
+    if (!genreExists) throw CustomError.notFound("Genre not found");
+
+    return genreExists;
+  }
+
+  private async validateGenreName(genreName: string) {
+    const genreExists = await prisma.genre.findFirst({
+      where: {
+        name: {
+          equals: genreName,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (genreExists)
+      throw CustomError.notFound("This genre name already exists");
+
+    return genreExists;
+  }
+
+  private async validateGenreRelations(genre_id: number) {
+    const moviesWithCurrentGenre = await prisma.movie.findMany({
+      where: {
+        genres: {
+          some: {
+            genre_id: genre_id,
+          },
+        },
+      },
+      include: {
+        genres: {
+          include: {
+            genre: true,
+          },
+        },
+      },
+    });
+
+    if (moviesWithCurrentGenre.length >= 15)
+      throw CustomError.badRequest(
+        "You cannot delete this genre because is highly correlated with multiple movies"
+      );
+
+    if (!moviesWithCurrentGenre.every((movie) => movie.genres.length > 1))
+      throw CustomError.badRequest(
+        "There are one or more movies that have only this genre. Add more genres or delete it"
+      );
   }
 }
