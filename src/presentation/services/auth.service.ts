9626c8e71a7e608ce496d34b45c7659dbb35c8b4
Martinchx <martincruz1426@gmail.com>
2024-02-18 02:32:06 -0300

    Added Service for Auth

---
+++ src/presentation/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { JWTAdapter } from "../../config";
+import { prisma } from "../../data/postgres";
+import { LoginUserDto, UserIdDto, CustomError } from "../../domain";
+
+export class AuthService {
+  constructor() {}
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const userExists = await this.validateUserExistence(loginUserDto.email);
+
+    if (userExists.password !== loginUserDto.password)
+      throw CustomError.badRequest("Email or password are wrong");
+
+    const token = await JWTAdapter.generateToken({
+      user_id: userExists.user_id,
+    });
+    if (!token) throw CustomError.internalServer("Error while creating JWT");
+
+    return { token };
+  }
+
+  async getCurrentSession(userIdDto: UserIdDto) {
+    const currentUserLogged = await prisma.user.findFirst({
+      where: {
+        user_id: userIdDto.user_id,
+      },
+    });
+
+    if (!currentUserLogged)
+      throw CustomError.notFound(
+        "User not found. Verify the credentials provided"
+      );
+
+    return { ...currentUserLogged };
+  }
+
+  private async validateUserExistence(user_email: string) {
+    const userExists = await prisma.user.findFirst({
+      where: {
+        email: user_email,
+      },
+    });
+
+    if (!userExists) throw CustomError.notFound("Email or password are wrong");
+
+    return userExists;
+  }
+}
--- src/presentation/services/index.ts
+++ src/presentation/services/index.ts
@@ -1,5 +1,6 @@
 /* Services */
 
+export * from "./auth.service";
 export * from "./user.service";
 export * from "./movie.service";
 export * from "./genre.service";
