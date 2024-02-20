5cf14097f82947f73c8f893802c76eb30168b913
Martinchx <martincruz1426@gmail.com>
2024-02-20 01:13:13 -0300

    WEBSERVICE_URL variable added

--- src/config/envs.adapter.ts
+++ src/config/envs.adapter.ts
@@ -3,5 +3,6 @@ import { get } from "env-var";
 
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
-  JWT_SEED: get("JWT_SEED").required().asString()
+  JWT_SEED: get("JWT_SEED").required().asString(),
+  WEBSERVICE_URL: get("WEBSERVICE_URL").required().asString(),
 };
