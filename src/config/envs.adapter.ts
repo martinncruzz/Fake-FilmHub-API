c485a912acccb701ed4221e069627f2d6b381517
Martín Cruz <martincruz1426@gmail.com>
2024-07-11 12:43:23 -0300

    DATABASE_URL env configured in envs adapter

--- src/config/envs.adapter.ts
+++ src/config/envs.adapter.ts
@@ -3,6 +3,7 @@ import { get } from "env-var";
 
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
-  JWT_SEED: get("JWT_SEED").required().asString(),
   WEBSERVICE_URL: get("WEBSERVICE_URL").required().asString(),
+  DATABASE_URL: get("DATABASE_URL").required().asUrlString(),
+  JWT_SEED: get("JWT_SEED").required().asString(),
 };
