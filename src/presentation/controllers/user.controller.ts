71df560fb3d6598e5ca3482a4dcb3a18df6def80
Martín Cruz <martincruz1426@gmail.com>
2024-09-19 15:39:54 -0300

    User service replaced with use-cases in user controller

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,26 +1,32 @@
 import { Request, Response } from "express";
 
-import { ErrorHandlerService, UserService } from "..";
 import {
+  CheckEmailAvailability,
+  CheckUserEmailDto,
+  CreateUser,
   CreateUserDto,
+  GetUserById,
+  GetUsers,
+  PaginationDto,
+  UpdateUser,
   UpdateUserDto,
   UserIdDto,
-  CheckUserEmailDto,
-  PaginationDto,
+  UserRepository,
 } from "../../domain";
+import { ErrorHandlerService } from "..";
 
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userRepository: UserRepository) {}
+
   getUsers = async (req: Request, res: Response) => {
     const { page = 1, limit = 10 } = req.query;
 
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
-
     if (error) return res.status(400).json({ error });
 
-    this.userService
-      .getUsers(paginationDto!)
-      .then((users) => res.status(200).json(users))
+    new GetUsers(this.userRepository)
+      .execute(paginationDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 
@@ -28,34 +34,31 @@ export class UserController {
     const { id } = req.params;
 
     const [error, userIdDto] = UserIdDto.get({ user_id: +id });
-
     if (error) return res.status(400).json({ error });
 
-    this.userService
-      .getUserById(userIdDto!)
-      .then((userFound) => res.status(200).json(userFound))
+    new GetUserById(this.userRepository)
+      .execute(userIdDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 
   createUser = async (req: Request, res: Response) => {
     const [error, createUserDto] = CreateUserDto.create(req.body);
-
     if (error) return res.status(400).json({ error });
 
-    this.userService
-      .createUser(createUserDto!)
-      .then((newUser) => res.status(200).json(newUser))
+    new CreateUser(this.userRepository)
+      .execute(createUserDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 
-  checkEmail = async (req: Request, res: Response) => {
+  checkEmailAvailability = async (req: Request, res: Response) => {
     const [error, checkUserEmailDto] = CheckUserEmailDto.create(req.body);
-
     if (error) return res.status(400).json({ error });
 
-    this.userService
-      .checkEmail(checkUserEmailDto!)
-      .then((isAvailable) => res.status(200).json(isAvailable))
+    new CheckEmailAvailability(this.userRepository)
+      .execute(checkUserEmailDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 
@@ -66,12 +69,11 @@ export class UserController {
       ...req.body,
       user_id: +id,
     });
-
     if (error) return res.status(400).json({ error });
 
-    this.userService
-      .updateUser(updateUserDto!)
-      .then((updatedUser) => res.status(200).json(updatedUser))
+    new UpdateUser(this.userRepository)
+      .execute(updateUserDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 }
--- src/presentation/routes/user.routes.ts
+++ src/presentation/routes/user.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
 
-import { UserController, UserService } from "..";
+import { UserController } from "..";
+import { UserDatasourceImpl, UserRepositoryImpl } from "../../infrastructure";
 
 export class UserRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const userService = new UserService();
-    const userController = new UserController(userService);
+    const userDatasource = new UserDatasourceImpl();
+    const userRepository = new UserRepositoryImpl(userDatasource);
+
+    const userController = new UserController(userRepository);
 
     router.get("/", userController.getUsers);
     router.get("/:id", userController.getUserById);
     router.post("/", userController.createUser);
-    router.post("/is-available", userController.checkEmail);
+    router.post("/is-available", userController.checkEmailAvailability);
     router.put("/:id", userController.updateUser);
 
     return router;
