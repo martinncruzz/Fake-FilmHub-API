7180756e6973400c3aeac09e4860b609b19310ab
Martín Cruz <martincruz1426@gmail.com>
2024-09-20 02:43:01 -0300

    Auth service replaced with use-cases in auth controller

--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -1,30 +1,52 @@
 import { Request, Response } from "express";
 
-import { AuthService, ErrorHandlerService } from "..";
-import { LoginUserDto, UserIdDto } from "../../domain";
+import { ErrorHandlerService } from "..";
+import {
+  AuthRepository,
+  CheckEmailAvailability,
+  CheckUserEmailDto,
+  GetCurrentSession,
+  LoginUser,
+  LoginUserDto,
+  RegisterUser,
+  RegisterUserDto,
+} from "../../domain";
 
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authRepository: AuthRepository) {}
 
-  loginUser = async (req: Request, res: Response) => {
-    const [error, loginUserDto] = LoginUserDto.create(req.body);
-
+  registerUser = async (req: Request, res: Response) => {
+    const [error, registerUserDto] = RegisterUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
 
-    this.authService
-      .loginUser(loginUserDto!)
-      .then((token) => res.status(200).json(token))
+    new RegisterUser(this.authRepository)
+      .execute(registerUserDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
   };
 
-  getCurrentSession = (req: Request, res: Response) => {
-    const [error, userIdDto] = UserIdDto.get(req.body);
-
+  loginUser = async (req: Request, res: Response) => {
+    const [error, loginUserDto] = LoginUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
 
-    this.authService
-      .getCurrentSession(userIdDto!)
-      .then((user) => res.status(200).json(user))
+    new LoginUser(this.authRepository)
+      .execute(loginUserDto!)
+      .then((data) => res.json(data))
       .catch((error) => ErrorHandlerService.handleError(error, res));
+  };
+
+  checkEmailAvailability = async (req: Request, res: Response) => {
+    const [error, checkUserEmailDto] = CheckUserEmailDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    new CheckEmailAvailability(this.authRepository)
+      .execute(checkUserEmailDto!)
+      .then((data) => res.json(data))
+      .catch((error) => ErrorHandlerService.handleError(error, res));
+  };
+
+  getCurrentSession = async (req: Request, res: Response) => {
+    const user = new GetCurrentSession().execute(req.body.user);
+    return res.json(user);
   };
 }
--- src/presentation/routes/auth.routes.ts
+++ src/presentation/routes/auth.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 
-import { AuthController, AuthMiddleware, AuthService } from "..";
+import { AuthController, AuthMiddleware } from "..";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const authService = new AuthService();
-    const authController = new AuthController(authService);
+    const authDatasource = new AuthDatasourceImpl();
+    const authRepository = new AuthRepositoryImpl(authDatasource);
 
+    const authController = new AuthController(authRepository);
+
+    router.post("/register", authController.registerUser);
     router.post("/login", authController.loginUser);
+    router.post("/is-available", authController.checkEmailAvailability);
     router.get(
       "/profile",
       [AuthMiddleware.validateJWT],
