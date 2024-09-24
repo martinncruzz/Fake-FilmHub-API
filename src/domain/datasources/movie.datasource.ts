8e12553b599dde3151a2995f1ec1e94a9073c350
Martín Cruz <martincruz1426@gmail.com>
2024-09-24 00:25:46 -0300

    Creating movie datasource and repository

---
+++ src/domain/datasources/movie.datasource.ts
@@ -0,0 +1,19 @@
+import {
+  CreateMovieDto,
+  MovieEntity,
+  MovieFiltersDto,
+  MovieIdDto,
+  PaginationDto,
+  UpdateMovieDto,
+} from "..";
+
+export abstract class MovieDatasource {
+  abstract getMovies(
+    paginationDto: PaginationDto,
+    movieFiltersDto: MovieFiltersDto
+  ): Promise<[number, MovieEntity[]]>;
+  abstract getMovieById(movieIdDto: MovieIdDto): Promise<MovieEntity>;
+  abstract createMovie(createMovieDto: CreateMovieDto): Promise<MovieEntity>;
+  abstract updateMovie(updateMovieDto: UpdateMovieDto): Promise<MovieEntity>;
+  abstract deleteMovie(movieIdDto: MovieIdDto): Promise<boolean>;
+}
