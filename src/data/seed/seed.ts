081a1edf6de53c1cb0cf63472c88de6e4a62f767
Martín Cruz <martincruz1426@gmail.com>
2024-09-28 16:55:33 -0300

    Seed script updated and improved

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,33 +1,71 @@
+import { Prisma } from "@prisma/client";
+
 import { prisma } from "../postgres";
 import { seedData } from "..";
 import { UserRole } from "../../domain/interfaces/shared/shared.interfaces";
 
 (async () => {
-  await main();
+  await seedDatabase();
 })();
 
-async function main() {
-  await prisma.$transaction([
-    prisma.userModel.deleteMany(),
-    prisma.movieGenreModel.deleteMany(),
-    prisma.genreModel.deleteMany(),
-    prisma.movieModel.deleteMany(),
+async function seedDatabase(): Promise<void> {
+  try {
+    await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+      await resetDatabaseSequences(tx);
+      await clearDatabase(tx);
+      await initializeDatabaseWithSeedData(tx);
+    });
+
+    console.log("Database seeding completed successfully.");
+  } catch (error) {
+    console.error("Error during database seeding:", error);
+  }
+}
+
+async function resetDatabaseSequences(
+  tx: Prisma.TransactionClient
+): Promise<void> {
+  await Promise.all([
+    tx.$executeRawUnsafe(
+      `ALTER SEQUENCE "UserModel_user_id_seq" RESTART WITH 1;`
+    ),
+    tx.$executeRawUnsafe(
+      `ALTER SEQUENCE "GenreModel_genre_id_seq" RESTART WITH 1;`
+    ),
+    tx.$executeRawUnsafe(
+      `ALTER SEQUENCE "MovieModel_movie_id_seq" RESTART WITH 1;`
+    ),
+  ]);
+}
+
+async function clearDatabase(tx: Prisma.TransactionClient): Promise<void> {
+  await Promise.all([
+    tx.movieGenreModel.deleteMany(),
+    tx.genreModel.deleteMany(),
+    tx.movieModel.deleteMany(),
+    tx.userModel.deleteMany(),
+  ]);
+}
+
+async function initializeDatabaseWithSeedData(
+  tx: Prisma.TransactionClient
+): Promise<void> {
+  await Promise.all([
+    tx.userModel.createMany({
+      data: seedData.users.map((user) => ({
+        ...user,
+        role: UserRole.USER,
+      })),
+    }),
+    tx.genreModel.createMany({
+      data: seedData.genres,
+    }),
   ]);
 
-  await prisma.$transaction([
-    prisma.userModel.createMany({
-      data: seedData.users.map((user) => ({ ...user, role: UserRole.USER })),
-    }),
-
-    prisma.genreModel.createMany({
-      data: seedData.genres.map((genre) => genre),
-    }),
-  ]);
-
-  for (const movie of seedData.movies) {
+  const moviesCreation = seedData.movies.map(async (movie) => {
     const { genre_ids, ...movieData } = movie;
 
-    await prisma.movieModel.create({
+    return tx.movieModel.create({
       data: {
         ...movieData,
         genres: {
@@ -37,7 +75,7 @@ async function main() {
         },
       },
     });
-  }
+  });
 
-  console.log("SEEDED");
+  await Promise.all(moviesCreation);
 }
