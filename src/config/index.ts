9861dedc46d8e04ea5662daaa77ad42d5dd2b24e
Martín Cruz <martincruz1426@gmail.com>
2024-10-10 17:55:24 -0300

    Barrel file of each layer updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,8 @@ export * from './schemas/genre/genre-dtos.schemas';
 export * from './schemas/genre/genre.schema';
 export * from './schemas/movie/movie-dtos.schemas';
 export * from './schemas/movie/movie.schema';
+export * from './schemas/review/review-dtos.schemas';
+export * from './schemas/review/review.schema';
 export * from './schemas/shared/filters.schemas';
 export * from './schemas/shared/shared.schemas';
 export * from './schemas/user/user-dtos.schemas';
