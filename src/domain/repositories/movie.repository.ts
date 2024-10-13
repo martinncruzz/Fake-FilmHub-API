42051459e5725deca8092c822d9b7af863b17f8f
Martín Cruz <martincruz1426@gmail.com>
2024-10-12 22:51:24 -0300

    Get reviews by movie method added in movie datasource and repository

--- src/domain/repositories/movie.repository.ts
+++ src/domain/repositories/movie.repository.ts
@@ -11,6 +11,7 @@ import {
 export abstract class MovieRepository {
   abstract getMovies(paginationDto: PaginationDto, movieFiltersDto: MovieFiltersDto): Promise<MoviesData>;
   abstract getMovieById(movieIdDto: MovieIdDto): Promise<MovieEntity>;
+  abstract getReviewsByMovie(movieIdDto: MovieIdDto): Promise<MovieEntity>;
   abstract createMovie(createMovieDto: CreateMovieDto): Promise<MovieEntity>;
   abstract updateMovie(updateMovieDto: UpdateMovieDto): Promise<MovieEntity>;
   abstract deleteMovie(movieIdDto: MovieIdDto): Promise<boolean>;
