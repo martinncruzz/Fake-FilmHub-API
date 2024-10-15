ce12ff931d607b79754e66f8c3b290dc80861646
Martín Cruz <martincruz1426@gmail.com>
2024-10-15 16:14:04 -0300

    Get reviews by user method added in user datasource and repository

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,7 +1,8 @@
-import { PaginationDto, UpdateUserDto, UserEntity, UserIdDto, UsersData } from '..';
+import { PaginationDto, UpdateUserDto, UserEntity, UserIdDto, UsersData, UserWithReviews } from '..';
 
 export abstract class UserRepository {
   abstract getUsers(paginationDto: PaginationDto): Promise<UsersData>;
   abstract getUserById(userIdDto: UserIdDto): Promise<UserEntity>;
+  abstract getReviewsByUser(userIdDto: UserIdDto, paginationDto: PaginationDto): Promise<UserWithReviews>;
   abstract updateUser(updateUserDto: UpdateUserDto): Promise<UserEntity>;
 }
