debf4be8c1415f1862d59ec2efcc8f5a05074dce
Martín Cruz <martincruz1426@gmail.com>
2024-10-31 16:51:44 -0300

    Creating auth.interfaces.ts

---
+++ src/domain/interfaces/features/auth.interfaces.ts
@@ -0,0 +1,17 @@
+export interface GoogleOAuthResponse {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
+  id_token: string;
+}
+
+export interface GoogleUserFromToken {
+  email: string;
+  email_verified: boolean;
+  name: string;
+  picture: string;
+  given_name: string;
+  family_name: string;
+}
