1c76c85cfdf3ac6c10d1dcfd9c542c4b5497fd03
Martín Cruz <martincruz1426@gmail.com>
2024-11-01 15:26:16 -0300

    Creating HandleOAuthCallbackUseCase and it implementation

---
+++ src/application/use-cases/auth/handle-oauth-callback.use-case.impl.ts
@@ -0,0 +1,40 @@
+import {
+  AuthService,
+  CustomError,
+  HandleOAuthCallbackUseCase,
+  HandleOAuthCallbackUseCaseResp,
+  OAuthProvider,
+  SignToken,
+  UserEntity,
+} from '../../../domain';
+import { JWTAdapter } from '../../../infrastructure';
+import { OAuthCallbackDto, OAuthProviderDto } from '../..';
+
+export class HandleOAuthCallbackUseCaseImpl implements HandleOAuthCallbackUseCase {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly signToken: SignToken = JWTAdapter.generateToken,
+  ) {}
+
+  async execute(
+    oauthProviderDto: OAuthProviderDto,
+    oauthCallbackDto: OAuthCallbackDto,
+  ): HandleOAuthCallbackUseCaseResp {
+    const { provider } = oauthProviderDto;
+    const { code } = oauthCallbackDto;
+    let user: UserEntity;
+
+    switch (provider) {
+      case OAuthProvider.GOOGLE:
+        user = await this.authService.authenticateWithGoogle(code);
+        break;
+      default:
+        throw CustomError.badRequest('Unhandled provider');
+    }
+
+    const token = await this.signToken({ user_id: user.user_id });
+    if (!token) throw CustomError.internalServer('Error generating token');
+
+    return { access_token: token };
+  }
+}
