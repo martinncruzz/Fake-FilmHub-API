80ccb06736a6ea04bdd530f13884ffeb5b7b1599
Martín Cruz <martincruz1426@gmail.com>
2024-11-08 15:10:35 -0300

    Facebook OAuth envs added

--- src/infrastructure/config/adapters/envs.adapter.ts
+++ src/infrastructure/config/adapters/envs.adapter.ts
@@ -6,7 +6,12 @@ export const envs = {
   WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
   DATABASE_URL: get('DATABASE_URL').required().asUrlString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
+
   GOOGLE_CLIENT_ID: get('GOOGLE_CLIENT_ID').required().asString(),
   GOOGLE_CLIENT_SECRET: get('GOOGLE_CLIENT_SECRET').required().asString(),
   GOOGLE_CALLBACK_URL: get('GOOGLE_CALLBACK_URL').required().asUrlString(),
+
+  FACEBOOK_CLIENT_ID: get('FACEBOOK_CLIENT_ID').required().asInt(),
+  FACEBOOK_CLIENT_SECRET: get('FACEBOOK_CLIENT_SECRET').required().asString(),
+  FACEBOOK_CALLBACK_URL: get('FACEBOOK_CALLBACK_URL').required().asUrlObject(),
 };
