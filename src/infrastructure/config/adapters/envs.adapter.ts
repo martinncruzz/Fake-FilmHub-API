e4238326487323381eed093ea0e3f7975bc2b143
Martín Cruz <martincruz1426@gmail.com>
2024-11-26 03:23:29 -0300

    Envs validation updated

--- src/infrastructure/config/adapters/envs.adapter.ts
+++ src/infrastructure/config/adapters/envs.adapter.ts
@@ -4,14 +4,14 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
-  DATABASE_URL: get('DATABASE_URL').required().asUrlString(),
+  DATABASE_URL: get('DATABASE_URL').required().asString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
 
   GOOGLE_CLIENT_ID: get('GOOGLE_CLIENT_ID').required().asString(),
   GOOGLE_CLIENT_SECRET: get('GOOGLE_CLIENT_SECRET').required().asString(),
-  GOOGLE_CALLBACK_URL: get('GOOGLE_CALLBACK_URL').required().asUrlString(),
+  GOOGLE_CALLBACK_URL: get('GOOGLE_CALLBACK_URL').required().asString(),
 
   FACEBOOK_CLIENT_ID: get('FACEBOOK_CLIENT_ID').required().asInt(),
   FACEBOOK_CLIENT_SECRET: get('FACEBOOK_CLIENT_SECRET').required().asString(),
-  FACEBOOK_CALLBACK_URL: get('FACEBOOK_CALLBACK_URL').required().asUrlString(),
+  FACEBOOK_CALLBACK_URL: get('FACEBOOK_CALLBACK_URL').required().asString(),
 };
