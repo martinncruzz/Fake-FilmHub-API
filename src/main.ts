bd89244c244bec47ca0bcdd648893cdc3f16a407
Martín Cruz <martincruz1426@gmail.com>
2025-01-28 15:45:10 -0300

    feat: initialize main application bootstrap with NestJS and Postgres connection

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { CustomExceptionFilter } from '@modules/shared/exceptions/custom-exception.filter';
+import { envs } from '@config/adapters/envs.adapter';
+import { PostgresDatabase } from '@database/postgres/postgres-database';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+
+  await PostgresDatabase.connect();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalFilters(new CustomExceptionFilter());
+
+  await app.listen(envs.PORT);
+  logger.log(`Server running on port ${envs.PORT}`);
+}
+bootstrap();
