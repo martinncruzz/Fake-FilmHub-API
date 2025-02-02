7f25746adf368ef6193515b58a7e23e55905d1c1
Martín Cruz <martincruz1426@gmail.com>
2025-02-02 01:22:21 -0300

    feat: add enums for UserRole and ResourceType

---
+++ src/modules/shared/interfaces/enums.ts
@@ -0,0 +1,11 @@
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+export enum ResourceType {
+  USERS = 'users',
+  MOVIES = 'movies',
+  GENRES = 'genres',
+  REVIEWS = 'reviews',
+}
