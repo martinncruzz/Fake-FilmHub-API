d102bed2b323afd67b86098943859868f2dacb4b
Martín Cruz <martincruz1426@gmail.com>
2025-02-03 17:23:44 -0300

    feat: implement UsersRepository with CRUD operations and pagination support

---
+++ src/modules/users/repositories/users.repository.impl.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+
+import { PaginationDto } from '@modules/shared/dtos/pagination.dto';
+import { PostgresDatabase } from '@database/postgres/postgres-database';
+import { RegisterUserDto } from '@modules/auth/dtos/register-user.dto';
+import { UpdateUserDto } from '@modules/users/dtos/update-user.dto';
+import { User } from '@modules/users/entities/user.entity';
+import { UserRole } from '@modules/shared/interfaces/enums';
+import { UsersRepository } from '@modules/users/repositories/users.repository';
+
+@Injectable()
+export class UsersRepositoryImpl implements UsersRepository {
+  private readonly prisma = PostgresDatabase.getClient();
+
+  async findAll(paginationDto: PaginationDto): Promise<{ total: number; users: User[] }> {
+    const { page, limit } = paginationDto;
+
+    const [total, users] = await this.prisma.$transaction([
+      this.prisma.user.count(),
+      this.prisma.user.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+    ]);
+
+    return { total, users: users.map(User.fromObject) };
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    return user ? User.fromObject(user) : null;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findFirst({ where: { email } });
+    return user ? User.fromObject(user) : null;
+  }
+
+  async create(registerUserDto: RegisterUserDto): Promise<User> {
+    const createdUser = await this.prisma.user.create({ data: { ...registerUserDto, role: UserRole.USER } });
+    return User.fromObject(createdUser);
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const updatedUser = await this.prisma.user.update({ where: { id }, data: updateUserDto });
+    return User.fromObject(updatedUser);
+  }
+}
---
+++ src/modules/users/repositories/users.repository.ts
@@ -0,0 +1,12 @@
+import { PaginationDto } from '@modules/shared/dtos/pagination.dto';
+import { RegisterUserDto } from '@modules/auth/dtos/register-user.dto';
+import { UpdateUserDto } from '@modules/users/dtos/update-user.dto';
+import { User } from '@modules/users/entities/user.entity';
+
+export abstract class UsersRepository {
+  abstract findAll(paginationDto: PaginationDto): Promise<{ total: number; users: User[] }>;
+  abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract create(registerUserDto: RegisterUserDto): Promise<User>;
+  abstract update(id: string, updateUserDto: UpdateUserDto): Promise<User>;
+}
